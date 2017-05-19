f6eabc2845a00103c172ebd43f4e29c6143cfb75
mobjohnson <mobjohnson@gmail.com>
2017-05-19 17:07:50 -0400

    Start select

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :organizations
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
