e2a92457163fc298cca4d6d28b7b96d3a15ccdd5
Keith Bennett <keithrbennett@gmail.com>
2017-05-19 10:30:48 -0400

    Seed User class.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'ffaker'
+
+
+NUM_USERS = 100
+
+NUM_USERS.times do
+  User.create!(
+      first_name: FFaker::Name.first_name,
+      last_name:  FFaker::Name.last_name,
+      address:    FFaker::Address.street_address,
+      city:       FFaker::Address.city,
+      state:      FFaker::AddressUS.state,
+      zipcode:    FFaker::AddressUS.zip_code,
+      email:      FFaker::Internet.email,
+      password:   'password',
+  )
+
+end
