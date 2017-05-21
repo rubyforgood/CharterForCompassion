dd747c828dd8de01aa4a68c983ef0f4bfc68d999
eric sabelhaus <eric@sabel.haus>
2017-05-21 11:48:54 -0400

    missed a schema change with the rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521134401) do
+ActiveRecord::Schema.define(version: 20170521141751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
