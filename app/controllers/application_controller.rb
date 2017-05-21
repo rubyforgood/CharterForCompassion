7be8568cc45e71aa284986afe836c74beb005341
Adam Bray <adam.bray@yale.edu>
2017-05-21 10:50:15 -0400

    Fix forms spacing and include interests/skills on user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:first_name, :last_name, :email, :address, :city, :state, :zipcode, :password, :password_confirmation]
+    added_attrs = [{skill_ids: [], interest_ids: [] }, :first_name, :last_name, :email, :address, :city, :state, :zipcode, :password, :password_confirmation]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
       user_params.permit({skill_ids: [], interest_ids: [] }, :first_name, :last_name, :email, :address, :city, :state, :zipcode, :password, :password_confirmation, :current_password)
