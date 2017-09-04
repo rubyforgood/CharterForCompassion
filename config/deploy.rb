d59d6a38f1fef25a0e60987f506322cd90322a9e
Sean Marcia <sean@cs.uoregon.edu>
2017-09-04 11:21:05 -0400

    Updates to the deploy.rb file to allow the capitrano console gem.

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/database.yml}
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
