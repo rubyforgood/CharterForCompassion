71f17a02655e89b8b1ac88ae6004017ef54834e0
mobjohnson <mobjohnson@gmail.com>
2017-10-08 17:33:13 -0400

    Move sleep

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
     sequence :email do |n|
-      "person#{n}@example.com"
       sleep 1
+      "person#{n}@example.com"
     end
     password 'password'
     first_name 'indy'
