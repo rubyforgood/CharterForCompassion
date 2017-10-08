4f8392d42a064adea415ca31bd55aa4f6433c2fc
mobjohnson <mobjohnson@gmail.com>
2017-10-08 15:32:44 -0400

    Add geocoder API key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,13 +1,13 @@
 Geocoder.configure(
   # Geocoding options
-  timeout: 3,                 # geocoding service timeout (secs)
+  timeout: 0.25,                 # geocoding service timeout (secs)
   lookup: :google,            # name of geocoding service (symbol)
   # ip_lookup: :freegeoip,      # name of IP address geocoding service (symbol)
   language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
+  api_key: AIzaSyA6s0IOazQh3mpPiJiRmzjV2Cm7it2iYeQ,               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
