4526ab1ff7998d8563c49d94615eab252ac06bd9
Admir23 <hello@admirkurtovic.com>
2017-10-22 16:05:00 +0200

    Add and edit skills

--- db/schema.rb
+++ db/schema.rb
@@ -132,14 +132,6 @@ ActiveRecord::Schema.define(version: 20171016211209) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "role_id"
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
-  end
-
   add_foreign_key "assignments", "roles"
   add_foreign_key "assignments", "users"
 end
